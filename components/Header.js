1ac4d22229f6310a67d48a6f510953e700bd8c75
Taibu Haroub <haroubtaibu@gmail.com>
2022-01-29 00:38:42 +0300

    Add header

---
+++ components/Header.js
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+
+function Header() {
+  return (
+    <header className="shadow-md border-b bg-white sticky top-0 z-50">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+        {/* left side  */}
+        {/* when using fill, make the parnet container relative */}
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            alt="main-logo"
+            objectFit="contain" // prevents image from being stretched
+          />
+        </div>
+
+        {/* will do the opposite of the above */}
+        {/* flex-shrink-0 => don't make the smaller as the screen shrinks keep it at specified size */}
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            alt="main-logo"
+            objectFit="contain" // prevents image from being stretched
+          />
+        </div>
+
+        {/* middle side- search input field */}
+        <div className="max-w-xs">
+          <div className="mt-1 relative p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              {/* will hold our search icon */}
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300
+                focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="nav-btn" />
+          <MenuIcon className="h-10 md:hidden cursor-pointer" />
+
+          <div className="relative nav-btn">
+            <PaperAirplaneIcon className="nav-btn rotate-45" />
+            <div
+              className="absolute -top-1 -right-2 text-xs w-5 h-5
+            bg-red-500 rounded-full flex justify-center items-center animate-pulse text-white"
+            >
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className="nav-btn" />
+          <UserGroupIcon className="nav-btn" />
+          <HeartIcon className="nav-btn" />
+
+          <img
+            src="https://links.papareact.com/3ke"
+            alt=""
+            loading="lazy"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -9,7 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-red-900">Instagram Clone</h1>
+      {/* Header  */}
+      <Header />
+
+      {/* Feed  */}
+
+      {/* Modal  */}
     </div>
   );
 }
