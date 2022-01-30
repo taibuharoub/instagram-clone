9bb987273c527a605d3837ecab41db2ad9529b1a
Taibu Haroub <haroubtaibu@gmail.com>
2022-01-30 03:51:56 +0300

    Add custom sigin page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,36 @@
 import { getProviders, signIn as signInToProvider } from "next-auth/react";
+import Header from "../../components/Header";
 
 function signIn({ providers }) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signInToProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+        <img
+          src="https://links.papareact.com/ocw"
+          alt=""
+          loading="lazy"
+          className="w-80 object-contain"
+        />
+        <p className="font-xs italic">
+          This is not a REAL app, it is built for educational purposes only
+        </p>
+
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() =>
+                  signInToProvider(provider.id, { callbackUrl: "/" })
+                }
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
