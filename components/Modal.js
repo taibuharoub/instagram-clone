cc53a6fba7f5dd18f49fcad1d307f71f8f90d479
Taibu Haroub <haroubtaibu@gmail.com>
2022-02-03 00:20:16 +0300

    Add modal and set logic to upload file

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,141 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
+import { Transition, Dialog } from "@headlessui/react";
+import { Fragment } from "react/cjs/react.production.min";
+import { CameraIcon } from "@heroicons/react/outline";
 
 function Modal() {
   const [open, setOpen] = useRecoilState(modalState);
-  return <div>{open && <p>The modal is open</p>}</div>;
+  const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const addImageToPost = (e) => {
+    // initize a file reader
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
+
+  // only show modal if open is true/available
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      {/* will get rendered out as a div => as="div" */}
+      <Dialog
+        as="div"
+        className="fixed z-10 inset-0 overflow-y-auto"
+        onClose={setOpen}
+      >
+        <div
+          className="flex items-end justify-center min-h-[800px] 
+        sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This is to trick the browser into centering the modal contents  */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div
+              className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-5 text-left
+            overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+            >
+              <div>
+                {selectedFile ? (
+                  <img
+                    src={selectedFile}
+                    alt=""
+                    onClick={() => setSelectedFile(null)}
+                    className="w-full object-contain cursor-pointer"
+                  />
+                ) : (
+                  <div
+                    onClick={() => filePickerRef.current.click()} // simulate a fake click
+                    className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                  >
+                    <CameraIcon
+                      className="h-6 w-6 text-red-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                )}
+
+                <div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      Upload a photo
+                    </Dialog.Title>
+
+                    <div>
+                      <input
+                        type="file"
+                        ref={filePickerRef}
+                        hidden
+                        onChange={addImageToPost}
+                      />
+                    </div>
+
+                    <div className="mt-2">
+                      <input
+                        className="border-none focus:ring-0 w-full text-center"
+                        type="text"
+                        ref={captionRef}
+                        placeholder="Please add a caption.."
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-transparent
+                      shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none
+                      focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300
+                      disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                  >
+                    Upload Post
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
 }
 
 export default Modal;
